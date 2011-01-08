3c93c2c3e28834edf2c87a6ec679a66a3441098f
hilmarch <hilmarch@5a44604e-c4f1-e921-e6b3-87e43fe012bd>
2011-01-08 07:09:27 +0000

    some shit

--- tig016-group13/Paperbin/LoginBox.java
+++ tig016-group13/Paperbin/LoginBox.java
@@ -1,8 +1,21 @@
 package gui;
 
 import java.awt.GridLayout;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import catalog.Movie;
 
 public class LoginBox extends JFrame
 {
@@ -37,4 +50,39 @@ public class LoginBox extends JFrame
 		this.setSize(400, 300);
 		this.setVisible(true);
 	}
+}
+
+@Override
+public List<Movie> getNewMoviesFromNetwork(URL url)
+{
+	List<Movie> movieList = new ArrayList<Movie>();
+	System.out.println("Försöker att läsa från nätverk.");
+	try
+	{
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder docBuilder;
+		docBuilder = docBuilderFactory.newDocumentBuilder();
+		InputStream stream = url.openStream();
+		if(stream.available()!=0)
+		{
+			Document doc = docBuilder.parse(stream);
+		    movieList = readDocument(doc);
+		}
+		else
+		{
+			System.out.println("Det går inte att läsa just nu. Filen är förmodligen tom!");
+		}
+	}
+	catch (ParserConfigurationException e)
+	{
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (SAXException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	return movieList;
 }
